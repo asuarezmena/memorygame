ea404ad5ef44f23bb524537e900e59559a6c86f8
asuarezmena <asuarezmena@gmail.com>
2026-02-17 23:13:55 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -59,12 +59,7 @@ async function cargarRanking(){
 
  
 
-const posiciones = [
- [100,50],[300,120],[500,180],[420,260],[200,320],
- [120,400],[300,460],[500,520],[420,600],[200,660],
- [120,740],[300,800],[500,850],[420,900],[200,950],
- [100,1000],[300,1060],[500,1120],[350,1180],[200,1240]
-];
+
 
 const cont = document.getElementById("nodes");
 
@@ -73,9 +68,6 @@ for(let i=1;i<=20;i++){
  const node = document.createElement("div");
  node.className="node";
  node.innerText=i;
-
- node.style.left = posiciones[i-1][0]+"px";
- node.style.top = posiciones[i-1][1]+"px";
 
  if(i==nivelActual){
    node.classList.add("active");
@@ -90,6 +82,7 @@ for(let i=1;i<=20;i++){
 
  cont.appendChild(node);
 }
+
 
 
 
