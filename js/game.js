8b48a40e9a26fb8eccfd611927ffaabc31e26c16
asuarezmena <asuarezmena@gmail.com>
2026-02-17 21:05:30 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,40 @@
 let nivelActual = 1;
 
+
+const posiciones = [
+ [100,50],[300,120],[500,180],[420,260],[200,320],
+ [120,400],[300,460],[500,520],[420,600],[200,660],
+ [120,740],[300,800],[500,850],[420,900],[200,950],
+ [100,1000],[300,1060],[500,1120],[350,1180],[200,1240]
+];
+
+const cont = document.getElementById("nodes");
+
+for(let i=1;i<=20;i++){
+
+ const node = document.createElement("div");
+ node.className="node";
+ node.innerText=i;
+
+ node.style.left = posiciones[i-1][0]+"px";
+ node.style.top = posiciones[i-1][1]+"px";
+
+ if(i==nivelActual){
+   node.classList.add("active");
+ }
+
+ node.onclick = ()=>{
+   if(i==nivelActual){
+     localStorage.setItem("level",i);
+     window.location="game.html";
+   }
+ }
+
+ cont.appendChild(node);
+}
+
+
+
 document.getElementById("userName").innerText =
 "Jugador: " + localStorage.getItem("user");
 
@@ -48,37 +83,7 @@ async function cargarRanking(){
 
 
 
-const posiciones = [
- [100,50],[300,120],[500,180],[420,260],[200,320],
- [120,400],[300,460],[500,520],[420,600],[200,660],
- [120,740],[300,800],[500,850],[420,900],[200,950],
- [100,1000],[300,1060],[500,1120],[350,1180],[200,1240]
-];
 
-const cont = document.getElementById("nodes");
-
-for(let i=1;i<=20;i++){
-
- const node = document.createElement("div");
- node.className="node";
- node.innerText=i;
-
- node.style.left = posiciones[i-1][0]+"px";
- node.style.top = posiciones[i-1][1]+"px";
-
- if(i==nivelActual){
-   node.classList.add("active");
- }
-
- node.onclick = ()=>{
-   if(i==nivelActual){
-     localStorage.setItem("level",i);
-     window.location="game.html";
-   }
- }
-
- cont.appendChild(node);
-}
 
 
 setInterval(cargarRanking,3000);
