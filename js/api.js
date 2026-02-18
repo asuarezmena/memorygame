5505f506e991d1765af6a1d432e758945df1d402
asuarezmena <asuarezmena@gmail.com>
2026-02-17 19:39:05 -0500

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,24 @@
+const API = "https://script.google.com/macros/s/AKfycbwgKBahWpCPTTlXuLaIVCQ4Vc9GMHSohwy8g1rukv9FUvS5CYCFnBknNxaDHJ2h1elu/exec";
+
+async function login(){
+  const username = document.getElementById("user").value;
+  const password = document.getElementById("pass").value;
+  
+  const res = await fetch(API,{
+    method:"POST",
+    body:JSON.stringify({
+      action:"login",
+      username,
+      password
+    })
+  });
+  
+  const data = await res.json();
+  
+  if(data.status=="ok"){
+    localStorage.setItem("user",username);
+    window.location="menu.html";
+  }else{
+    alert("login incorrecto");
+  }
+}
