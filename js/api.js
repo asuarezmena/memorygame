0a161f1148b5468f7c0c1d33545d67fe36b558f8
asuarezmena <asuarezmena@gmail.com>
2026-02-17 22:06:49 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,19 +1,13 @@
-const API = "https://script.google.com/macros/s/AKfycbwXAji7P0rsVF4To1M4_KPJSXHgV5qBajNsG4YFzFAsmldL_Oi6l7P3UU7vsR2Kwl84/exec"; 
+const API = "https://script.google.com/macros/s/AKfycbwXAji7P0rsVF4To1M4_KPJSXHgV5qBajNsG4YFzFAsmldL_Oi6l7P3UU7vsR2Kwl84/exec";
 
 async function login(){
     const username = document.getElementById("user").value;
     const password = document.getElementById("pass").value;
 
-    const res = await fetch(API,{
-        method:"POST",
-        body: new URLSearchParams({
-            data: JSON.stringify({
-                action:"login",
-                username:username,
-                password:password
-            })
-        })
-    });
+    const res = await fetch(
+        API + "?action=login&username=" + encodeURIComponent(username) +
+        "&password=" + encodeURIComponent(password)
+    );
 
     const data = await res.json();
 
@@ -22,28 +16,5 @@ async function login(){
         window.location="menu.html";
     }else{
         alert("Login incorrecto");
-    }
-}
-
-async function register(){
-    const username = document.getElementById("user").value;
-    const password = document.getElementById("pass").value;
-
-    const res = await fetch(API,{
-        method:"POST",
-        body: new URLSearchParams({
-            data: JSON.stringify({
-                action:"register",
-                username:username,
-                password:password
-            })
-        })
-    });
-
-    const data = await res.json();
-
-    if(data.status=="ok"){
-        alert("Cuenta creada");
-        window.location="login.html";
     }
 }
