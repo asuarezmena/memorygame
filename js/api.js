736f0cceb6c06ff3042b12d3f9ed828e607a2f91
asuarezmena <asuarezmena@gmail.com>
2026-02-18 23:58:01 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -15,7 +15,7 @@ async function login(){
         localStorage.setItem("user",username);
         window.location="menu.html";
     }else{
-        alert("Login incorrecto");
+        showModal("Login incorrecto", "error");
     }
 }
 
@@ -34,9 +34,9 @@ async function register(){
     const data = await res.json();
 
     if(data.status=="ok"){
-        alert("Usuario creado correctamente");
+        showModal("Usuario creado correctamente", "success");
         window.location="index.html";
     }else{
-        alert(data.message || "Error al registrar");
+        showModal(data.message || "Error al registrar", "error");
     }
 }
