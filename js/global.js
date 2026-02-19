7e231473f092583066d54132882ff43aa608b6a1
asuarezmena <asuarezmena@gmail.com>
2026-02-18 23:50:03 -0500

    Update global.js

--- js/global.js
+++ js/global.js
@@ -6,3 +6,29 @@ function cerrarSesion(){
     localStorage.clear();
     window.location = "login.html";
 }
+
+
+function showModal(message, type = "normal") {
+
+    const modal = document.getElementById("customModal");
+    const content = document.getElementById("modalContent");
+    const text = document.getElementById("modalMessage");
+
+    text.textContent = message;
+
+    content.classList.remove("success", "error");
+
+    if(type === "success"){
+        content.classList.add("success");
+    }
+
+    if(type === "error"){
+        content.classList.add("error");
+    }
+
+    modal.classList.add("show");
+
+    setTimeout(() => {
+        modal.classList.remove("show");
+    }, 2000); // 2 segundos
+}
